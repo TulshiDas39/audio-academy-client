08a114e54c9d10e6ffc5d38a9dfe184a7a6e2dd5
tulshi das <bsse0811@iit.du.ac.bd>
2020-11-30 22:16:18 +0600

    handle submit

--- src/components/common/modals/registerContributorModal.tsx
+++ src/components/common/modals/registerContributorModal.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { EnumModals } from '../../../lib';
 import { useSelectorTyped } from '../../../store/rootReducer';
 import { ActionsModal } from './reducers';
+
+interface IFormData{
+  name:string,
+  email:string,
+  phone:string,
+}
 
 function RegisterContributorModalComponent(){
     const dispatch = useDispatch();
     const store = useSelectorTyped((state)=>({
         show: state.modals.openModals.includes(EnumModals.REGISTER_CONTRIBUTOR),
     }))
+
+    const {errors, register, handleSubmit} = useForm<IFormData>({
+      mode:"onSubmit",
+      reValidateMode:"onChange",
+    })
     
-    const closeModal=()=>{
+    const onClose=()=>{
       dispatch(ActionsModal.hideModal(EnumModals.REGISTER_CONTRIBUTOR));
+    }
+
+    const onSubmit=(data:IFormData)=>{
+      debugger;
     }
 
     return (
@@ -22,16 +38,24 @@ function RegisterContributorModalComponent(){
         centered
         show = {store.show}
     >
-      <Modal.Header closeButton>
+      <Modal.Header closeButton onHide={onClose}>
         <p>Register Contributor</p>
       </Modal.Header>
       <Modal.Body>
-        <Form>
-            
+        <Form id="registerContributorForm" onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group>
+              <Form.Control name="name" type="text" placeholder="Full name" ref={register({required:"Full name is required"})}/>
+              <p className="text-danger">{errors.name?.message || ''}</p>
+              <Form.Control name="phone" type="text" placeholder="Phone" ref={register({required:"Phone is required"})}/>
+              <p className="text-danger">{errors.phone?.message || ''}</p>
+              <Form.Control name="email" type="text" placeholder="Email" ref={register({required:"Email is required"})}/>
+              <p className="text-danger">{errors.email?.message || ''}</p>
+            </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={()=> dispatch(ActionsModal.hideModal(EnumModals.REGISTER_CONTRIBUTOR))}>Close</Button>
+        <Button variant="success" type="submit" form={"registerContributorForm"}>Submit</Button>
+        <Button variant="danger" onClick={onClose} >Close</Button>
       </Modal.Footer>
     </Modal>
     )
