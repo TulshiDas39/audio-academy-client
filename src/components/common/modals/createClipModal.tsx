7ae012d3e255481961a69383db156c30357cccec
tulshi das <bsse0811@iit.du.ac.bd>
2021-01-01 01:37:42 +0600

    add contributor in clip

--- src/components/common/modals/createClipModal.tsx
+++ src/components/common/modals/createClipModal.tsx
@@ -1,10 +1,12 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useEffect } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
-import { EnumModals, useMultiState } from '../../../lib';
-import { ITutorialEntity } from '../../../lib/types/entities';
+import useSWR from 'swr';
+import { ApiRoutes, EnumModals, useMultiState } from '../../../lib';
+import { IEntityUser, ITutorialEntity } from '../../../lib/types/entities';
 import { useSelectorTyped } from '../../../store/rootReducer';
+import { ApiGetAllContributors } from '../../contributors/api';
 import { ApiCreateClip, ApiSearchTutorial, ICreateClipPayload } from './api';
 import { ActionsModal } from './reducers';
 
@@ -17,14 +19,20 @@ interface IFormData{
 
 interface IState{
   tutorialName:string;
+  contributorSearchKey:string;
   selectedTutorial: ITutorialEntity;
+  selectedContributor:IEntityUser;
   tutorialSuggestions: ITutorialEntity[];
+  contributorSuggestions:IEntityUser[];
 }
 
 const initialState={
   tutorialName:"",
+  contributorSearchKey:"",
   tutorialSuggestions:[],
   selectedTutorial:null!,
+  selectedContributor:null!,
+  contributorSuggestions:[],
 } as IState;
 
 let preventBlur = false;
@@ -42,14 +50,21 @@ function CreateClipModalComponent(){
       reValidateMode:"onChange",
     })
     
+    useEffect(()=>{
+      if(!store.show) setState(initialState);
+     
+    },[store.show])
+    const {data} = useSWR(ApiRoutes.AllContributors, ApiGetAllContributors)
     const onClose=()=>{
       dispatch(ActionsModal.hideModal(EnumModals.CREATE_CLIP));
     }
 
     const onSubmit=(data: IFormData)=>{
+    const tutorialId = window.location.pathname.split("/").pop();
+      if(!tutorialId) return;
       ApiCreateClip({
         ...data,
-        tutorialId:state.selectedTutorial._id,
+        tutorialId: tutorialId,
       }).then(res=>{
         if(res.response){
           onClose();
@@ -77,6 +92,14 @@ function CreateClipModalComponent(){
       });
     }
 
+    const handleContributorSearch=(e:ChangeEvent<HTMLInputElement>)=>{
+      setState({contributorSearchKey:e.target.value});
+      if(!e.target.value) setState({contributorSuggestions:[]})
+      else setState({
+        contributorSuggestions: data?.response?.data.filter(x=>x.name.startsWith(e.target.value) || x.email.startsWith(e.target.value))
+      })
+    }
+
     return (
     <Modal
         size="lg"
@@ -94,7 +117,7 @@ function CreateClipModalComponent(){
               <p className="text-danger">{errors.title?.message || ''}</p>
               <Form.Control name="lession" type="text" placeholder="Lesson" ref={register({required:"Lesson is required"})}/>
               <p className="text-danger">{errors.lession?.message || ''}</p>
-              <div>
+              {/* <div>
                 <Form.Control type="text" value={state.tutorialName} placeholder="Select Tutorial" 
                   onBlur={()=>!preventBlur && setState({tutorialSuggestions:[]})}
                   onChange={handleTutorialSearch}
@@ -111,7 +134,7 @@ function CreateClipModalComponent(){
                     ))
                   }
                 </div>
-              </div>
+              </div> */}
               <Form.Control name="description" 
                 type="textarea" placeholder="Description" 
                 as={"textarea"} rows={3} 
@@ -122,6 +145,25 @@ function CreateClipModalComponent(){
               <p className="text-danger">{errors.description?.message || ''}</p>
               <Form.Control name="deadline" type={"text"} placeholder="Deadline for contributor" ref={register()}/>
               <p className="text-danger">{errors.deadline?.message || ''}</p>
+
+               <div>
+                <Form.Control type="text" value={state.contributorSearchKey} placeholder="Select Contributor" 
+                  onBlur={()=>!preventBlur && setState({contributorSuggestions:[]})}
+                  onChange={handleContributorSearch}
+                />
+                <div className="">
+                  {
+                    state.contributorSuggestions.map(t=>(
+                      <div className="border rounded py-1 cur-point"
+                       onMouseDown={_ => preventBlur = true}
+                       onMouseUp={_=> preventBlur = false}
+                       onClick={_=>setState({selectedContributor:t,contributorSearchKey:t.name,contributorSuggestions:[]})}>
+                        {t.name}
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
             </Form.Group>
         </Form>
       </Modal.Body>
