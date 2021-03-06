8078219f1a924ec515d5ca2164758615a4e1713b
tulshi das <bsse0811@iit.du.ac.bd>
2021-03-06 15:57:04 +0000

    filter clips in admin view

--- src/components/singleTutorialDetails/singleTutorialDetails.tsx
+++ src/components/singleTutorialDetails/singleTutorialDetails.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useMemo } from 'react';
+import { Button, ButtonGroup, ToggleButton } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import useSWR from 'swr';
-import { EnumModals } from '../../lib';
+import { EnumModals, useMultiState } from '../../lib';
 import { ActionsModal } from '../common/modals';
 import { apiGetSingleTutorialDetails } from './api';
 import { Clip } from './subComponents/singleClip';
 
+type FilterButtonName = "All"|"Submitted"|"Not Submitted"
+
+interface IFilterButton{
+    name:FilterButtonName;
+    id:string;
+}
+const filterButtons:IFilterButton[]=[
+    {
+        id:"all",
+        name:"All",
+    },
+    {
+        id:"submitted",
+        name:"Submitted",
+    },
+    {
+        id:"Not_Submitted",
+        name:"Not Submitted"
+    }
+]
+interface IState{
+    selectedFilterButton:IFilterButton;    
+}
+const initialState:IState={
+    selectedFilterButton:filterButtons[0],
+}
 function SingleTutorialDetailsComponent(){
     const dispatch = useDispatch();
+    const [state,setState]=useMultiState(initialState);
     const tutorialId = window.location.pathname.split("/").pop();
     const {data} = useSWR(tutorialId!,()=>apiGetSingleTutorialDetails(tutorialId!).then(res=>{
         if(res.response) return res.response.data;
         throw res.error;
     }));
+    const filteredClips = useMemo(()=>{
+        if(state.selectedFilterButton.name === "All")return data?.clips;
+        if(state.selectedFilterButton.name === "Submitted") return data?.clips.filter(x=>!!x.submissionDate);
+        if(state.selectedFilterButton.name === "Not Submitted") return data?.clips.filter(x=>!x.submissionDate);
+    },[data?.clips,state.selectedFilterButton])
     return (
         <div>
             <div>
@@ -22,13 +54,31 @@ function SingleTutorialDetailsComponent(){
                 <div className="text-right">
                     <Button onClick={()=>  dispatch(ActionsModal.showModal(EnumModals.CREATE_CLIP))}>Add Clip</Button>
                 </div>
+                <div>
+                    <ButtonGroup toggle>
+                        {filterButtons.map((radio, idx) => (
+                            <ToggleButton
+                                key={idx}
+                                type="radio"
+                                variant="secondary"
+                                name="radio"
+                                value={radio.name}
+                                checked={state.selectedFilterButton.id === radio.id}
+                                // onChange={(e) => setRadioValue(e.currentTarget.value)}
+                                onClick={()=>setState({selectedFilterButton:radio})}
+                            >
+                                {radio.name}
+                            </ToggleButton>
+                        ))}
+                    </ButtonGroup>
+                </div>
             </div>
             <hr/>
             <div>
                 <h6 className="mb-3">Clips</h6>
                 <div>
                     {
-                        data?.clips.map(x=>(
+                        filteredClips?.map(x=>(
                             <Clip key={x._id} clip={x}/>
                         ))
                     }
